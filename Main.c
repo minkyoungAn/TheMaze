0a51b611549604c19ddc31a044f3751420332022
oraora <yeopung@naver.com>
2018-11-21 10:49:09 +0900

    게임 클리어 후 play_time 초기화

--- Main.c
+++ Main.c
@@ -206,7 +206,7 @@ void Touch_ISR()
 /* ---------------------------------------------------------------------------- */
 
 unsigned char game_play; //1ready:2play:3clear:4restart
-extern unsigned char play_time;
+
 int Main(void)
 {
     unsigned int  score = 0;
@@ -230,8 +230,7 @@ int Main(void)
 		Display_bitmap();
     	make_maze();
     	display_maze();
-
-	    play_time = 0;
+    	
 		game_play = 2;
 	    while(1)
 	    {
