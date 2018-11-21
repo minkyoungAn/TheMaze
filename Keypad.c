45551f2d31988898d3643479b85eccc244bde483
minkyoungAn <minkyoungan@gmail.com>
2018-11-21 09:56:31 +0900

    key interrupt

--- Keypad.c
+++ Keypad.c
@@ -12,13 +12,7 @@ void Key_EINT2_ISR(void)
 	rINTMSK1 |= (0x7<<2);
 	rINTMSK1  |= (0x1<<10);
 
-	if(rGPFDAT == 0xff){
-		Uart_Printf("key 1 pushed!!!\n");
-	}
-	else{
-		Uart_Printf("key 6 pushed!!!\n");
-	}
-
+	//Uart_Printf("1\n");
 
 	rSRCPND1 |= (0x7<<2);
 	rINTPND1 |= (0x7<<2);
@@ -29,14 +23,9 @@ void Key_EINT3_ISR(void)
 {
 	rINTMSK1 |= (0x7<<2);
 	rINTMSK1  |= (0x1<<10);
-	if(rGPFDAT == 0xff){
-		Uart_Printf("key 2 pushed!!!\n");
-	}
-	else {
-		Uart_Printf("key 7 pushed!!!\n");
-	}
-	
 
+	//Uart_Printf("2\n");
+	
 	rSRCPND1 |=(0x7<<2);
 	rINTPND1 |=(0x7<<2);
 	rINTMSK1&=~(0x7<<2); 
@@ -47,37 +36,23 @@ void Key_EINT4_6_ISR(void)
 	rEINTMASK |= 0x7<<4; // masking 
 	rINTMSK1 |= (0x7<<2);
 	rINTMSK1  |= (0x1<<10);	
-
+	#if 0
 	if(rEINTPEND == 0x10){
-		if( rGPFDAT == 0xff ){
-			Uart_Printf("key 3 pushed!!!\n");
-		}
-		else {
-			Uart_Printf("key 8 pushed!!!\n");
-		}
+		Uart_Printf("3\n");
+		
 	}
 	else if(rEINTPEND == 0x20){
-		if( rGPFDAT == 0xff ){
-			Uart_Printf("key 4 pushed!!!\n");
-		}
-		else {
-			Uart_Printf("key 9 pushed!!!\n");
-		}
+		Uart_Printf("4\n");
+		
 	}
 	else if(rEINTPEND == 0x40){
-		if( rGPFDAT == 0xff ){
-			Uart_Printf("key 5 pushed!!!\n");
-		}
-		else {
-			Uart_Printf("key 10 pushed!!!\n");
-		}
+		Uart_Printf("5\n");
+		
 	}
-
+	#endif
 	rEINTPEND |= 0x3<<4; // pending	
 	rSRCPND1 |=(0x3<<2);
 	rINTPND1 |= (0x3<<2);
-
-	
 
 	rEINTMASK &= ~0x7<<4; // not masking 
 	rINTMSK1&=~(0x7<<2);
@@ -92,6 +67,7 @@ void Init_Key(void)
 	pISR_EINT4_7 = Key_EINT4_6_ISR;
 
 	/* GPFCON - EINT Mode GPF [2:6] */
+	// 1. GPF2~6 interrupt enable
 	rGPFCON |= (0x2aa<<4);
 	rEXTINT0 |= 0x3<<8;
 	rEXTINT0 |= 0x3<<12;
@@ -99,23 +75,15 @@ void Init_Key(void)
 	rEXTINT0 |= 0x3<<20;
 	rEXTINT0 |= 0x3<<24;
 	
+	// 3. GPF7, GPG0 LOW(0)
+    rGPFDAT &= ~(0x1<<7);
+	rGPGDAT &= ~(0x1<<0);
+
 	/* EINT [2:6] */
 	rINTMOD1 &= ~ 0x7<<2;
 	rINTMSK1 &= ~ 0x7<<2;
 	rEINTMASK &= ~ 0x7<<4;
 	
-	#if 0
-    // 1. GPF2~6를 input으로 설정
-    rGPFCON &= ~(0x3ff << 4);
-	
-    // 2. GPF7, GPG0을 output으로 설정
-	rGPFCON = (rGPFCON & ~(0x3<<14)) | (0x1<<14);
-	rGPGCON = (rGPGCON & ~(0x3<<0)) | (0x1<< 0);
-	
-    // 3. GPF7, GPG0에 LOW(0) 출력
-    rGPFDAT &= ~(0x1<<7);
-	rGPGDAT &= ~(0x1<<0);
-	#endif
 }
 
 int Get_Key_byPolling(void)
